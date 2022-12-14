63d3ca3205cfea1450dcbf5417cbb8f29f00f0c7
Đoàn Thành Lợi <100418351+ThanhLoi-300@users.noreply.github.com>
2022-12-15 03:37:30 +0700

    Update UC_Bill.cs

--- GUI/UC_Bill.cs
+++ GUI/UC_Bill.cs
@@ -30,18 +30,22 @@ namespace GUI
         {
 
             lb_nameOder.Text = bill_BUS.get_Name_Product(data.Product_Id) + " (" + data.Size + ")";
-            lbl_Quantity_X_Price.Text = data.Quantity + " X " + data.Price.ToString("#,#,#") + "đ";
-            double total = data.Price * data.Quantity;
-            lb_priceOder.Text = total.ToString("#,#,#") + "đ";
+            
             if (data.Percent > 0)
             {
                 guna2CircleButton1.Text = "-" + data.Percent + "%";
-                label1.Text = (discountBUS.get_Price_Product(data.Product_Id) + discountBUS.get_Price_Product(data.Product_Id) * 0.05).ToString("#,#,#") + "đ";
+                lbl_Quantity_X_Price.Text = data.Quantity + " X " + (data.Price - data.Price * data.Percent/100).ToString("#,#,#") + "đ";
+                label1.Text = data.Price.ToString("#,#,#") + "đ";
+                double total = (data.Price - data.Price * data.Percent / 100) * data.Quantity;
+                lb_priceOder.Text = total.ToString("#,#,#") + "đ";
             }
             else
             {
                 guna2CircleButton1.Visible = false;
                 label1.Visible = false;
+                lbl_Quantity_X_Price.Text = data.Quantity + " X " + data.Price.ToString("#,#,#") + "đ";
+                double total = data.Price * data.Quantity;
+                lb_priceOder.Text = total.ToString("#,#,#") + "đ";
             }
         }
     }
